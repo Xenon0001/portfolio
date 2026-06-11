acc9eb00c6be78c484182157e39ca952741608f3
user.name <luisrafaeleyoma764@gmail.com>
2026-06-11 12:53:21 +0100

    fix: eliminar imports de next-intl en Blog y otros componentes

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
+import { useTranslations } from '@/lib/useTranslations';
 
 export default function Blog() {
   const t = useTranslations('blog');
